0a30bd4ea6ec5f13ddddd070a52d46b409cc66f9
ravi <ravivarmanb025@gmail.com>
2025-06-25 17:33:42 +0530

    Initial commit

---
+++ src/main/java/com/example/bloodtypeidentifier/BloodtypeidentifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloodtypeidentifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodtypeidentifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodtypeidentifierApplication.class, args);
+	}
+
+}
